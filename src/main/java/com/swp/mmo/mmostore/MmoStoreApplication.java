02caffac60c9d489b3fc7c467825be45cf460b7f
huynq <huynguyensteph@gmail.com>
2025-09-13 16:03:06 +0700

    Initial commit

---
+++ src/main/java/com/swp/mmo/mmostore/MmoStoreApplication.java
@@ -0,0 +1,13 @@
+package com.swp.mmo.mmostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmoStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmoStoreApplication.class, args);
+    }
+
+}
