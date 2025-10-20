f3cdea49fead58aa1ed2374e6cd6b53fc5b3fa84
ShiroHoang <136798761+ShiroHoang@users.noreply.github.com>
2025-10-20 10:17:30 +0700

    Hoang [Order]: checkout order

--- src/main/java/com/swp/mmostore/service/OrderService.java
+++ src/main/java/com/swp/mmostore/service/OrderService.java
@@ -1,14 +1,40 @@
 package com.swp.mmostore.service;
 
 import com.swp.mmostore.entity.Order;
+import com.swp.mmostore.entity.User;
 import com.swp.mmostore.repository.OrderRepository;
+import com.swp.mmostore.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+
+    private final UserRepository userRepository;
+
+    public Order createNewOrder(int quantity, double totalPrice, String userEmail) {
+        Order order = new Order();
+        order.setQuantity(quantity);
+        order.setTotalPrice(BigDecimal.valueOf(totalPrice));
+
+        //get user
+        Integer userId = userRepository.findByEmail(userEmail).getUserId();
+
+        order.setCreateBy(userId);
+        order.setCreateAt(LocalDateTime.now());
+
+        User user = new User();
+        user.setUserId(userId);
+        order.setUser(user);
+
+        return createPendingOrder(order);
+    }
+
 
     public Order createPendingOrder(Order order){
         order.setStatus("PENDING");
